4eb912773db9beb4f6cc5a8dfc1c97450621b823
SarafNusrat <saraf.nusrat11@gmail.com>
2022-06-06 17:56:10 +0600

    double comfort

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
                   <h1 className="font-bold text-4xl mt-14 pt-14 mb-2 text-blue-200">GRAB YOUR BEST SHOES</h1>
                   <h1 className="font-bold text-5xl mt-4 mb-2 text-orange-600">TODAY</h1>
                   <p className="text-gray-700 text-base ml-4 pr-16 pl-16 mt-6">
-                  Nike's first lifestyle Air Max brings you style, comfort and big attitude in the Nike Air Max 270. The design draws inspiration from Air Max icons showcasing Nike's greatest innovation.
+                  Nike's first lifestyle Air Max brings you style, comfort, comfort and big attitude in the Nike Air Max 270. The design draws inspiration from Air Max icons showcasing Nike's greatest innovation.
                     </p>
                 </div>
                 <div>
